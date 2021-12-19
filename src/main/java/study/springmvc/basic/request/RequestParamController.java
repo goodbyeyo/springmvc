43c0fee17b5ce74d07fe2b373b62f45e3059535a
yoonseol <yoonseol1983@gmail.com>
2021-12-19 10:19:35 +0900

    HTTP 요청메세지 String

--- src/main/java/study/springmvc/basic/request/RequestParamController.java
+++ src/main/java/study/springmvc/basic/request/RequestParamController.java
@@ -2,9 +2,11 @@ package study.springmvc.basic.request;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import study.springmvc.basic.HelloData;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -118,6 +120,49 @@ public class RequestParamController {
         log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
         return "ok";
     }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@RequestParam String username, @RequestParam int age) {
+        HelloData helloData = new HelloData();
+        helloData.setUsername(username);
+        helloData.setAge(age);
+
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        log.info("helloData={}", helloData);
+        return "ok";
+    }
+
+    /**
+     * 스프링MVC는 @ModelAttribute 가 있으면 다음을 실행한다.
+     * HelloData 객체를 생성한다.
+     * 요청 파라미터의 이름으로 HelloData 객체의 프로퍼티를 찾는다.
+     * 그리고 해당 프로퍼티의 setter를 호출해서 파라미터의 값을 입력(바인딩) 한다.
+     * age=abc 처럼 숫자가 들어가야 할 곳에 문자를 넣으면 BindException 이 발생한다 => 예외처리 필수
+     */
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(@ModelAttribute HelloData helloData) {
+
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+
+        return "ok";
+    }
+
+    /**
+     * @ModelAttribute 생략 할 수 있다
+     * String , int , Integer 같은 단순 타입 = @RequestParam
+     * 나머지 = @ModelAttribute (argument resolver 로 지정해둔 타입 외)
+     */
+    @ResponseBody
+    @RequestMapping("/model-attribute-v3")
+    public String modelAttributeV3(HelloData helloData) {
+
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+
+        return "ok";
+    }
+
 
 
 }
